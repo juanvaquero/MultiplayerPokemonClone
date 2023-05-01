d702c7ac709af4c2970f06e140f29b04922492ba
juanvaquero <juan_arnau@hotmail.es>
2023-05-01 03:04:22 +0200

    refactor: Remove unnused player name in lobby

--- Assets/Scripts/UI/Network/UINetworkMenu.cs
+++ Assets/Scripts/UI/Network/UINetworkMenu.cs
@@ -7,8 +7,6 @@ public class UINetworkMenu : MonoBehaviourPunCallbacks
     public const string GAME_SCENE = "Game";
 
     [SerializeField]
-    private UIPanelNetwork _playerPanel;
-    [SerializeField]
     private UIPanelNetwork _createRoomPanel;
     [SerializeField]
     private UIPanelNetwork _joinRoomPanel;
@@ -17,19 +15,15 @@ public class UINetworkMenu : MonoBehaviourPunCallbacks
     {
         _createRoomPanel.OkButton.onClick.AddListener(CreateRoom);
         _joinRoomPanel.OkButton.onClick.AddListener(JoinRoom);
-
-        _playerPanel.InputField.text = "Player" + Random.Range(1, 20);
     }
 
     public void CreateRoom()
     {
-        PhotonNetwork.LocalPlayer.NickName = _playerPanel.InputField.text;
         PhotonNetwork.CreateRoom(_createRoomPanel.InputField.text);
     }
 
     public void JoinRoom()
     {
-        PhotonNetwork.LocalPlayer.NickName = _playerPanel.InputField.text;
         PhotonNetwork.JoinRoom(_joinRoomPanel.InputField.text);
     }
 
